e7f28c2bc33f9188e6d5a1dedecbd35b4d9f1f64
endo <endo@elplanning.co.jp>
2026-09-17 19:35:30 +0900

    feat:ユーザー入力をCSVに変換する

--- csv_app/index.js
+++ csv_app/index.js
@@ -1,10 +1,45 @@
-import { writeFileSync } from "fs";
+import { appendFileSync } from "fs";
+import { createInterface } from "readline";
 
-const content = "Test content!";
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-try {
-  writeFileSync("./test.txt", content);
-  console.log("Success!");
-} catch (err) {
-  console.log(err);
+const readLineAsync = (question) => {
+    return new Promise((resolve) => readline.question(question, resolve));
+};
+
+class Person {
+    constructor(name = "", number = "", email = "") {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+    saveToCSV() {
+        const content = `${this.name},${this.number},${this.email}\n`;
+        try {
+            appendFileSync("./contacts.csv", content);
+            console.log(`${this.name} Saved!`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 }
+
+const startApp = async () => {
+    let shouldContinue = true;
+    while (shouldContinue) {
+        const name = await readLineAsync("Enter your Name: ");
+        const number = await readLineAsync("Enter your Number: ");
+        const email = await readLineAsync("Enter your Email: ");
+        const person = new Person(name, number, email);
+        await person.saveToCSV();
+
+        const response = await readLineAsync("Do you want to add another contact? (y/n): ");
+        shouldContinue = response.toLowerCase() === "y";
+    }
+    readline.close();
+};
+
+startApp();
