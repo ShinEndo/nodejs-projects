71a1c7585865dddbd7ec6e138c2fc60607210e8c
endo <endo@elplanning.co.jp>
2026-09-21 16:47:38 +0900

    feat:フィードの読み取りと解析

--- food_feeds_rss_app/index.js
+++ food_feeds_rss_app/index.js
@@ -1,6 +1,12 @@
+import Parser from "rss-parser";
+const parser = new Parser();
 const main = async () => {
   const url = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
-  const response = await fetch(url);
-  console.log(await response.text());
+  const {title,items} = await parser.parseURL(url);
+  console.log(title);
+  const results = items.map(({title, link}) => ({title, link}));
+  console.clear();
+  console.table(results);
+  console.log('Last updated', (new Date()).toUTCString() );
 }
-main();
+setInterval(main,2000);
