113c5c0c4a19db69fbf51ecd2807f1681b2e8b73
endo <endo@elplanning.co.jp>
2026-09-21 23:21:10 +0900

    feat:データベースの接続とデータの視覚化

--- sentiment_journal/index.js
+++ sentiment_journal/index.js
@@ -1,51 +1,10 @@
-import Spellchecker from "spellchecker";
-import natural from "natural";
-import prompt from 'prompt';
-import { removeStopwords } from 'stopword';
-prompt.start({});
-prompt.message = '';
-const tokenizer = new natural.WordTokenizer();
+import SentimentJournal from "./sentimentJournal.js";
 
-const correctSpelling = inputString => {
-  const words = inputString.split(' ');
-  const corrections = [];
-  for(let word of words) {
-    if(Spellchecker.isMisspelled(word)) {
-      const options = Spellchecker.getCorrectionsForMisspelling(word);
-      corrections.push(options[0]);
-    } else {
-      corrections.push(word);
-    }
-  }
-  return corrections.join(' ');
+const journal = new SentimentJournal();
+await journal.fetchEntries();
+
+while(true) {
+  await journal.printChart();
+  await journal.promptEntry();
+  await journal.analyzeSentiment();
 }
-
-const tokenizeInput = (inputString) => {
-  return tokenizer.tokenize(inputString);
-}
-
-const stemWords = tokens => {
-  const stems = [];
-  for(let token of tokens) {
-    const stem = natural.PorterStemmer.stem(token);
-    stems.push(stem);
-  }
-  return stems;
-}
-
-// (async () => {
-  try {
-    const {inputString} = await prompt.get({
-      name: 'inputString',
-      description: 'How do you feel?',
-    });
-    const correctedSpelling = correctSpelling(inputString);
-    const tokens = tokenizeInput(correctedSpelling);
-    const { SentimentAnalyzer, PorterStemmer } = natural;
-    const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
-    const sentimentResults = analyzer.getSentiment(tokens);
-    console.log(sentimentResults);
-  } catch(e) {
-    console.error(`An error occured: ${e.message}`);
-  }
-// })();
