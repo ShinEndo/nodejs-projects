31c40f9846f11b9886f4ad28cc3243f5d5da944b
endo <endo@elplanning.co.jp>
2026-09-21 17:11:21 +0900

    feat:アグリゲーターにカスタム項目を追加する

--- food_feeds_rss_app/index.js
+++ food_feeds_rss_app/index.js
@@ -1,5 +1,8 @@
+import promptModule from "prompt-sync";
 import Parser from "rss-parser";
+const prompt = promptModule({sigint: true});
 const parser = new Parser();
+const customItems = [];
 const main = async () => {
   const urls = [
     "https://www.bonappetit.com/feed/recipes-rss-feed/rss",
@@ -21,8 +24,11 @@ const aggregate = (responses, feedItems) => {
   }
 }
 const print = feedItems => {
+  const res = prompt('Add item: ');
+  const [title, link] = res.split(',');
+  if(![title, link].includes(undefined)) customItems.push({title, link});
   console.clear();
-  console.table(feedItems);
+  console.table(feedItems.concat(customItems));
   console.log('Last updated', (new Date()).toUTCString() );
 }
 setInterval(main,2000);
