6f2fcc861b5ff6326cc66cf0c9dc6b31816ba2c6
endo <endo@elplanning.co.jp>
2026-09-21 17:01:47 +0900

    feat:アグリゲーターを作成する

--- food_feeds_rss_app/index.js
+++ food_feeds_rss_app/index.js
@@ -1,12 +1,28 @@
 import Parser from "rss-parser";
 const parser = new Parser();
 const main = async () => {
-  const url = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
-  const {title,items} = await parser.parseURL(url);
-  console.log(title);
-  const results = items.map(({title, link}) => ({title, link}));
+  const urls = [
+    "https://www.bonappetit.com/feed/recipes-rss-feed/rss",
+    "https://www.budgetbytes.com/category/recipes/feed/",
+  ];
+  const feedItems = [];
+  const awaitableRequests = urls.map(url => parser.parseURL(url));
+  const responses = await Promise.all(awaitableRequests);
+  aggregate(responses, feedItems);
+  print(feedItems);
+}
+const aggregate = (responses, feedItems) => {
+  for(let {items} of responses) {
+    for(let {title, link} of items) {
+      if(title.toLowerCase().includes('chicken')) {
+        feedItems.push({title,link});
+      }
+    }
+  }
+}
+const print = feedItems => {
   console.clear();
-  console.table(results);
+  console.table(feedItems);
   console.log('Last updated', (new Date()).toUTCString() );
 }
 setInterval(main,2000);
