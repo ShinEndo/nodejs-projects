e692575001609edb46c628279aee010364b72bb0
endo <endo@elplanning.co.jp>
2026-09-23 16:29:19 +0900

    feat:データベースに接続する

--- marketing_mailer/index.js
+++ marketing_mailer/index.js
@@ -1,7 +1,8 @@
 import Fastify from "fastify";
 import formBody from "@fastify/formbody";
-import { welcomeMail } from "./mailTemplates.js";
+import { welcomeMail, confirmationMail } from "./mailTemplates.js";
 import { sendMail } from "./services/mailer.js";
+import Lead from "./db.js";
 
 const app = Fastify();
 await app.register(formBody);
@@ -12,8 +13,29 @@ app.post("/subscribe", async (request, reply) => {
   const { email = process.env.GMAIL_TO } = request.body;
   console.log(`Received ${email}`);
 
-  await sendMail(email, welcomeMail());
+  try {
+    await Lead.create({ email });
+    await sendMail(email, welcomeMail());
+  } catch(e) {
+    console.error("Could not save the Lead:", e.message);
+  }
   reply.send({ message: "okay" });
+});
+
+app.get("/verify/:email", async (request,reply) => {
+  const { email } = request.params;
+  try {
+    const lead = await Lead.findOne({ where: { email } });
+    if(lead) {
+      lead.verified = true,
+      await lead.save();
+      console.log(`${email} is vefified`);
+      await sendMail(email, confirmationMail());
+    }
+  } catch(e) {
+    console.error("Could not verify the Lead:", e.message);
+    reply.send({ message: "Unbale to verify." });
+  }
 });
 
 try {
--- marketing_mailer/mailTemplates.js
+++ marketing_mailer/mailTemplates.js
@@ -11,3 +11,8 @@ export const welcomeMail = () => {
   const content = `<h1>Welcome to Inn Box!</h1>`;
   return htmlTemplates(content);
 }
+
+export const confirmationMail = (url) => {
+  const content = `<a href="${url}"><h1>Confrim your email</h1></a>`;
+  return htmlTemplates(content);
+}
