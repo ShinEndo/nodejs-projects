320609353b6042124e46e0512226fa617666f49b
endo <endo@elplanning.co.jp>
2026-09-23 21:49:45 +0900

    feat:ユーザーアカウントの保存と保護

--- app_authentication/index.js
+++ app_authentication/index.js
@@ -2,6 +2,10 @@ import Fastify from "fastify";
 import fastifyFormbody from "@fastify/formbody";
 import fastyfyView from "@fastify/view";
 import handlebars from "handlebars";
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+import fastifyPassport from "@fastify/passport";
+import Account from "./models/Account.js"
 
 const app = Fastify();
 const PORT = 3000;
@@ -13,6 +17,27 @@ await app.register(fastyfyView, {
   engine: { handlebars },
   root: "views",
 });
+await app.register(fastifyCookie);
+await app.register(fastifySession, {
+  secret: "a_very_secret_value_1!2@3#4$5%6^7&8*9(0)",
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+  savaUninitialized: false,
+  resave: false,
+});
+await app.register(fastifyPassport.initialize());
+await app.register(fastifyPassport.secureSession());
+fastifyPassport.registerUserSerializer(async (user, request) => user.username);
+fastifyPassport.registerUserDeserializer(async (user,request) => {
+  const account = await Account.findByUsername(username);
+  if(!account) {
+    throw new Error("User not found");
+  }
+  return account;
+})
+fastifyPassport.use("local", Account.genStrategy());
 
 const loginFormVars = {
   signup: {
@@ -41,16 +66,35 @@ const users = {};
 
 app.post("/account", async (request,reply) => {
   const { username, password } = request.body;
-  users[username] = password;
-  return reply.send({ message: "Account created" });
+  try {
+    await Account.register(username, password);
+    return reply.send({ message: "Account created." });
+  } catch(e) {
+    return reply.code(400).send({
+      massage: "Account creation failed.",
+      error: e.message,
+    });
+  }
 });
 
 app.post("/auth", async (request, reply) => {
-  const { username, password } = request.body;
-  if(users[username] && users[username] === password) {
-    return reply.send({ message: "Logged in" });
-  }
-  return reply.redirect("/?page=login");
+  return fastifyPassport.authenticate(
+    "local",
+    { authInfo: false},
+    async (request, reply, err, user) => {
+      if(err || !user) {
+        return reply.code(401).send({
+          message: "Authentication failed",
+          error: "Invalid username or password",
+        });
+      }
+      await request.logIn(user);
+      return reply.send({
+        message: "Logged in",
+        username: user.username
+      });
+    } 
+  )(request, reply);
 });
 
 try {
