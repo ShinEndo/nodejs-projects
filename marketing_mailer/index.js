a17603f555c10623a280a87f771b04a96b8f34ba
endo <endo@elplanning.co.jp>
2026-09-23 16:52:40 +0900

    feat:メールエンゲージメントのためのマーケティングピクセルを実現する

--- marketing_mailer/index.js
+++ marketing_mailer/index.js
@@ -1,8 +1,10 @@
+import dotenv from "dotenv";
 import Fastify from "fastify";
 import formBody from "@fastify/formbody";
-import { welcomeMail, confirmationMail } from "./mailTemplates.js";
+import { welcomeMail, confirmationMail, campaignMail } from "./mailTemplates.js";
 import { sendMail } from "./services/mailer.js";
 import Lead from "./db.js";
+dotenv.config();
 
 const app = Fastify();
 await app.register(formBody);
@@ -36,6 +38,27 @@ app.get("/verify/:email", async (request,reply) => {
     console.error("Could not verify the Lead:", e.message);
     reply.send({ message: "Unbale to verify." });
   }
+});
+
+app.get("/campaign/:campainKey/user/:email/image.png", async (request,reply) => {
+  const { email, campainKey } = request.params;
+  try {
+    const lead = await Lead.findOne({ where: { email } });
+    if(lead) {
+      lead.lastCampain = campainKey;
+      await lead.save();
+      console.log(`${email} opened ${campainKey}`);
+    }
+  } catch(e) {
+    console.error("An error occured", e.message);
+  }
+  reply.send({ message: "okay" });
+} );
+
+sendMail(process.env.GMAIL_USER,campaignMail("Special Promotion", "promo1", process.env.GMAIL_TO));
+
+app.ready(() => {
+  console.log(app.printRoutes());
 });
 
 try {
--- marketing_mailer/mailTemplates.js
+++ marketing_mailer/mailTemplates.js
@@ -16,3 +16,11 @@ export const confirmationMail = (url) => {
   const content = `<a href="${url}"><h1>Confrim your email</h1></a>`;
   return htmlTemplates(content);
 }
+
+export const campaignMail = (campaignText, campaignKey, email) => {
+  const content = `
+  <h1>${campaignText}</h1>
+  <img src="http://localhost:3000/campaign/${campaignKey}/user/${email}/image.png" style="display:none">
+  `;
+  return htmlTemplates(content);
+}
