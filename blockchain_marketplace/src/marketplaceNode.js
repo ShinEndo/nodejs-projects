f58b1cec63c64f5a6560c0f1e732149d615922e7
endo <endo@elplanning.co.jp>
2026-09-25 19:06:39 +0900

    feat:ブロックチェーンのコーディング

--- blockchain_marketplace/src/marketplaceNode.js
+++ blockchain_marketplace/src/marketplaceNode.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Transaction from "./transaction.js";
 
 class MarketplaceNode {
   constructor(url, peers = [], blockchain){
@@ -27,6 +28,55 @@ class MarketplaceNode {
   async registerNode(newNodeUrl) {
     this.peers.push(newNodeUrl);
     await this.broadcast("sync-peers", { peers: this.peers });
+    await this.broadcastBlockchain();
+  }
+  async broadcastBlockchain() {
+    await this.broadcast("sync-blockchain", {
+      chain: this.blockchain.chain,
+      songs: this.songs,
+    })
+  }
+  async processTransaction(transactionHash) {
+    const transaction = new Transaction(transactionHash);
+    const id = transactionHash.id || transaction.id;
+    if(transaction.transactionType === "BUY") {
+      if(this.balance < transaction.price) return "Insufficient balance";
+      this.balance -= transaction.price;
+      try {
+        await axios.post(`${transaction.recipient}/payment`, {
+          price: transaction.price
+        });
+      } catch(e) {
+        console.log(e.message);
+      }
+      console.log({ bal: this.balance });
+    }
+    this.songs[id] = transaction;
+    this.blockchain.pendingTransactions.push(transaction);
+    if(this.blockchain.pendingTransactions > 4) {
+      console.log(await this.mine());
+    }
+    await this.broadcastBlockchain();
+    return "Processed transaction"
+  }
+  async mine() {
+    const price = this.blockchain.miningReward;
+    const reward = new Transaction({
+      sender: this.peers[0],
+      recipient: this.url,
+      price,
+      transactionType: "MINE",
+    });
+
+    this.blockchain.pendingTransactions.push(reward);
+    await this.blockchain.mineBlock();
+    this.balance += price;
+    return "Mining complete";
+  }
+  availableSongs() {
+    return Object.values(this.songs)
+      .filter((transaction) => transaction.transactionType === "SELL")
+      .map(({ id, songTitle, price} ) => [id, songTitle, price]);
   }
 }
 export default MarketplaceNode;
